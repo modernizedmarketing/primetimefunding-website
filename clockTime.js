c036499f3a29209d1b5ddfd7a0dcf77d7c2dd03f
asafe <asafe.kauffmann@gmail.com>
2023-12-22 15:28:40 -0300

    Timer

---
+++ clockTime.js
@@ -0,0 +1,27 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const newYears = '31 December 2023';
+
+function countDown() {
+
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  
+  const totalSeconds = (newYearsDate - currentDate) /1000;
+  const days = Math.floor(totalSeconds /3600/ 24);
+  const hours = Math.floor(totalSeconds /3600) % 24; 
+  const minutes = Math.floor(totalSeconds/60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+
+  daysEl.innerText = days;
+  hoursEl.innerText  = hours;
+  minsEl.innerText = minutes;
+  secondsEl.innerText = seconds;
+
+} setInterval(countDown, 1000);
+console.log(newYearsDate)
