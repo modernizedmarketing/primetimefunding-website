476c3419017c0c7167982320a4bdb5c2abce0428
Eddy Aguirre <eddyj.aguirre@gmail.com>
2024-01-03 13:54:08 -0400

    Change date of countdown

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
-const newYears = '31 December 2023';
+const releaseDate = '2024-01-27T10:00:00';
 
 function countDown() {
-  const newYearsDate = new Date(newYears);
+  const releaseDateDate = new Date(releaseDate);
   const currentDate = new Date();
-  const totalSeconds = (newYearsDate - currentDate) /1000;
+  const totalSeconds = (releaseDateDate - currentDate) /1000;
   const days = Math.floor(totalSeconds /3600/ 24);
   const hours = Math.floor(totalSeconds /3600) % 24; 
   const minutes = Math.floor(totalSeconds/60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
-  daysEl.innerText = days;
-  hoursEl.innerText  = hours;
-  minsEl.innerText = minutes;
-  secondsEl.innerText = seconds;
+  if (days >= 0 && hours >= 0 && minutes >= 0 && seconds >= 0) 
+  {
+    daysEl.innerText = days;
+    hoursEl.innerText  = hours;
+    minsEl.innerText = minutes;
+    secondsEl.innerText = seconds;
+  }
 
 } setInterval(countDown, 1000);
 
